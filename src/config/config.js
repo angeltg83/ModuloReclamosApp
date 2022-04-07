48adea82df07d5533add7825f178f6852212751f
Angel Tigua <atigua@romerodyasociados.com>
2022-04-07 11:09:13 -0500

    cambios

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export const config = {
   API_PORT: 8000,
-  BASE_URL: "http://localhost",
+  BASE_URL: "http://192.168.0.176",
 
   get API_HOST() {
     return this.BASE_URL + ":" + this.API_PORT;
